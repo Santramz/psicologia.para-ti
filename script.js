321efcec2b62e210e0d76645be760221bf8e5b3a
Santramz <s.antramzbunny82@gmail.com>
2025-10-02 17:09:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 submitButton.disabled = false;
                 
                 // Redirect to WhatsApp (optional)
-                const numeroDestino = '525638263959';
+                const numeroDestino = '5638263959';
                 const message = encodeURIComponent(`Hola Erika, soy ${nombre}\n` +, `Teléfono/WhatsApp: ${whatsapp}` . Estoy interesado(a) en la evaluación gratuita de TCC. Mi email es ${email}.`);
                 const whatsappUrl = `https://wa.me/${numeroDestino}?text=${message}`;
                 
